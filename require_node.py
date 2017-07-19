da72ff261d737d8a67fb003ddb42a7852f4a2808
Constantin Titarenko <constantin.titarenko@gmail.com>
2017-07-19 21:04:30 +0300

    add support for vue files

--- require_node.py
+++ require_node.py
@@ -49,6 +49,10 @@ class RequireNodeCommand(sublime_plugin.TextCommand):
                 "text.html.riot": {
                     True:  settings.get('source_js_new_line'),
                     False: settings.get('source_js_existing_line')
+                },
+                "text.html.vue": {
+                    True:  settings.get('source_js_new_line'),
+                    False: settings.get('source_js_existing_line')
                 }
             }
 
